b3805f28e682a9fa1837302c3dafeafb4abc5fe0
ikaldiaz <ikaldiaz@gmail.com>
2019-08-26 06:57:49 +0700

    adding pickaday.js

--- footer.php
+++ footer.php
@@ -45,6 +45,14 @@
 
             });
 
+        var picker = new Pikaday({
+            field: document.getElementById('tgl'),
+            format: 'D MMM YYYY',
+            onSelect: function() {
+                console.log(this.getMoment().format('Do MMMM YYYY'));
+            }
+        });
+
 	});
 
 
